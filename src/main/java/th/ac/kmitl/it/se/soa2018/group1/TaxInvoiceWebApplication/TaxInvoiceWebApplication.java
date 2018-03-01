f7e944a3efa6e96975f5a590445858bbd6c1e3c7
armhansa <armhansa@gmail.com>
2018-03-01 17:29:04 +0700

    Initialize Spring Boot Project

---
+++ src/main/java/th/ac/kmitl/it/se/soa2018/group1/TaxInvoiceWebApplication/TaxInvoiceWebApplication.java
@@ -0,0 +1,12 @@
+package th.ac.kmitl.it.se.soa2018.group1.TaxInvoiceWebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxInvoiceWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxInvoiceWebApplication.class, args);
+	}
+}
---
+++ src/test/java/th/ac/kmitl/it/se/soa2018/group1/TaxInvoiceWebApplication/TaxInvoiceWebApplicationTests.java
@@ -0,0 +1,16 @@
+package th.ac.kmitl.it.se.soa2018.group1.TaxInvoiceWebApplication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaxInvoiceWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
