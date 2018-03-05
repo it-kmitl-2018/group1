ffccca4238162526454a3f4cd808401867770fa5
armhansa <armhansa@gmail.com>
2018-03-05 15:49:57 +0700

    Delete unrelated initialize Project

--- src/main/java/th/ac/kmitl/it/se/soa2018/group1/TaxInvoiceWebApplication/TaxInvoiceWebApplication.java
+++ src/main/java/th/ac/kmitl/it/se/soa2018/group1/TaxInvoiceWebApplication/TaxInvoiceWebApplication.java
@@ -15,16 +15,4 @@ public class TaxInvoiceWebApplication {
 		SpringApplication.run(TaxInvoiceWebApplication.class, args);
 	}
 
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-			System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-		};
-	}
 }
