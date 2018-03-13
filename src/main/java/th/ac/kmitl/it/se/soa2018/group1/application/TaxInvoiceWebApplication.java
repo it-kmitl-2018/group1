1708bdb1ea9f5fecf47b81be8bcd7f1de4629f77
dnyopr <dnyopr@gmail.com>
2018-03-13 12:46:54 +0700

    Create architecture example

--- src/main/java/th/ac/kmitl/it/se/soa2018/group1/application/TaxInvoiceWebApplication.java
+++ src/main/java/th/ac/kmitl/it/se/soa2018/group1/application/TaxInvoiceWebApplication.java
@@ -1,12 +1,7 @@
-package th.ac.kmitl.it.se.soa2018.group1.TaxInvoiceWebApplication;
+package th.ac.kmitl.it.se.soa2018.group1.application;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class TaxInvoiceWebApplication {
