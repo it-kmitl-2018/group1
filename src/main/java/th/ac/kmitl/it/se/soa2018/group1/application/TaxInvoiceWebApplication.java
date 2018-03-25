cbb3bad294dbc3ff99734c9b204a69f7abf2280d
armhansa <armhansa@gmail.com>
2018-03-26 01:21:58 +0700

    Disable Security Password to Testable

--- src/main/java/th/ac/kmitl/it/se/soa2018/group1/application/TaxInvoiceWebApplication.java
+++ src/main/java/th/ac/kmitl/it/se/soa2018/group1/application/TaxInvoiceWebApplication.java
@@ -2,11 +2,12 @@ package th.ac.kmitl.it.se.soa2018.group1.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class TaxInvoiceWebApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(TaxInvoiceWebApplication.class, args);
 	}
 
