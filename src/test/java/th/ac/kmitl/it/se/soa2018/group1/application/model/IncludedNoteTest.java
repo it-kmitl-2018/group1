e1830370e3fa5dea512b0e911ae304502f81b40f
armhansa <armhansa@gmail.com>
2018-04-01 21:32:51 +0700

    Add Included Note model and test

---
+++ src/test/java/th/ac/kmitl/it/se/soa2018/group1/application/model/IncludedNoteTest.java
@@ -0,0 +1,22 @@
+package th.ac.kmitl.it.se.soa2018.group1.application.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IncludedNoteTest {
+
+    private final String SUBJECT = "หมายเหตุ";
+    private final String CONTENT = "กรณีเสนอขายใบสำคัญแสดงสิทธิอนุพันธ์ ให้รายงานผลการขายภายใน 45 วันนับแต่วันปิดขาย";
+
+    @Test
+    public void checkInputCorrectData() {
+        IncludedNote includedNote = new IncludedNote();
+        includedNote.setSubject(this.SUBJECT);
+        includedNote.setContent(this.CONTENT);
+
+        assertEquals(this.SUBJECT, includedNote.getSubject());
+        assertEquals(this.CONTENT, includedNote.getContent());
+    }
+
+}
