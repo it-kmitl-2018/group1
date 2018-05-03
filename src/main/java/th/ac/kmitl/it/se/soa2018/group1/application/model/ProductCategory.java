449b8785b12ab81aa892b3fbb7b34438d5a6736f
dnyopr <dnyopr@gmail.com>
2018-05-03 22:05:50 +0700

    Add attribute in Product Category Model class

--- src/main/java/th/ac/kmitl/it/se/soa2018/group1/application/model/ProductCategory.java
+++ src/main/java/th/ac/kmitl/it/se/soa2018/group1/application/model/ProductCategory.java
@@ -1,4 +1,12 @@
 package th.ac.kmitl.it.se.soa2018.group1.application.model;
 
-public class ProductCategory {
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+public @Data class ProductCategory {
+
+    private String code;
+    private String name;
+
 }
