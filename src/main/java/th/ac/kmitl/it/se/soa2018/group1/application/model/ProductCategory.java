577ac84d88b03544e696b2c3a360b0cdc6d46ece
dnyopr <dnyopr@gmail.com>
2018-05-03 21:45:24 +0700

    Create Product Category model and test

---
+++ src/main/java/th/ac/kmitl/it/se/soa2018/group1/application/model/ProductCategory.java
@@ -0,0 +1,4 @@
+package th.ac.kmitl.it.se.soa2018.group1.application.model;
+
+public class ProductCategory {
+}
---
+++ src/test/java/th/ac/kmitl/it/se/soa2018/group1/application/model/ProductCategoryTest.java
@@ -0,0 +1,25 @@
+package th.ac.kmitl.it.se.soa2018.group1.application.model;
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class ProductCategoryTest {
+
+    private final String code = "14111507";
+    private final String name = "กระดาษถ่ายเอกสารหรือพิมพ์งานทั่วไป";
+
+    @Test
+    public void checkInputCorrectData(){
+
+        ProductCategory productCategory = ProductCategory.builder()
+                .code(this.code)
+                .name(this.name)
+                .build();
+
+        assertEquals(this.code, productCategory.getCode());
+        assertEquals(this.name, productCategory.getName());
+
+    }
+
+}
