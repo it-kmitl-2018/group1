0b0a807f59d5d415d3476562acf977500ab3a098
piyapa <58070084@kmtil.ac.th>
2018-05-14 13:09:28 +0700

    Create JsonAddress

---
+++ src/test/java/th/ac/kmitl/it/se/soa2018/group1/application/json/JsonAddressTest.java
@@ -0,0 +1,37 @@
+package th.ac.kmitl.it.se.soa2018.group1.application.json;
+
+import org.junit.Test;
+
+import  static  org.junit.Assert.assertEquals;
+
+class JsonAddressTest {
+
+    private String lineOne = "สำนักงานใหญ่ เลขที่ 313/1";
+    private String lineTwo = "ถนนรัชดาภิเษก แขวงบุคลโล เขตธนบุรี กรุงเทพมหานคร 10600";
+    private String cityName = "ธนบุรี";
+    private String citySubDivisionName = "บุคคโล";
+    private String postCode = "10600";
+    private String countrySubDivision = "กรุงเทพมหานคร";
+    private String countryName = "TH";
+
+    @Test
+    public void shouldGetAllInfo() {
+        JsonAddress jsonAddress = JsonAddress.builder()
+                .lineOne(this.lineOne)
+                .lineTwo(this.lineTwo)
+                .cityName(this.cityName)
+                .citySubDivisionName(this.citySubDivisionName)
+                .postCode(this.postCode)
+                .countrySubDivision(this.countrySubDivision)
+                .countryName(this.countryName)
+                .build();
+
+        assertEquals(this.lineOne , jsonAddress.lineOne);
+        assertEquals(this.lineTwo , jsonAddress.lineTwo);
+        assertEquals(this.cityName , jsonAddress.cityName);
+        assertEquals(this.citySubDivisionName , jsonAddress.citySubDivisionName);
+        assertEquals(this.postCode , jsonAddress.postCode);
+        assertEquals(this.countrySubDivision , jsonAddress.countrySubDivision);
+        assertEquals(this.countryName , jsonAddress.countryName);
+    }
+}
